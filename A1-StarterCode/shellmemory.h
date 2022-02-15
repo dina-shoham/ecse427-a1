3554bc46f247ed29749739e6d3dd8bc4231c2066
sepehr moalemi <64672710+SepehrMoalemi@users.noreply.github.com>
2022-02-14 23:45:37 -0500

    Update shellmemory.h

--- A1-StarterCode/shellmemory.h
+++ A1-StarterCode/shellmemory.h
@@ -1,3 +1,7 @@
-void mem_init();
+// Extra
+int   isEqualto(char *str1, char *str2);
+
+// Given
+void  mem_init();
 char *mem_get_value(char *var);
-void mem_set_value(char *var, char *value);
+void  mem_set_value(char *var, char *value);
